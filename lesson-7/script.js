0e221b525d24a3125e1baa4077ebb6a0159d28ac
xrsQugi <02.04.5.art@gmail.com>
2023-01-02 11:34:25 +0200

    update

--- lesson-7/script.js
+++ lesson-7/script.js
@@ -1,10 +1,9 @@
-console.log(Math.round(Math.random() * 10));
-console.log(Math.round(Math.random() * 10));
-console.log(Math.round(Math.random() * 10));
-console.log(Math.round(Math.random() * 10));
-console.log(Math.round(Math.random() * 10));
-console.log(Math.round(Math.random() * 10));
-console.log(Math.round(Math.random() * 10));
-console.log(Math.round(Math.random() * 10));
-console.log(Math.round(Math.random() * 10));
-console.log(Math.round(Math.random() * 10));
+// console.log(Math.round(Math.random() * 10));
+// console.log(Math.random() * (10 - 1) + 1);
+// console.log(Math.round(Math.random() * (max - min) + min));
+
+let min = Number.parseInt(prompt("Выбери от какого числа будет работать генератор чисел"));
+let max = Number.parseInt(prompt("Выбери до какого числа будет работать генератор чисел"));
+alert(Math.round(Math.random() * (max - min) + min));
+
+
