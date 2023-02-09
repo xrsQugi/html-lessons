27aae4cc40c287563b934aa49a48d2c53a28f419
xrsQugi <02.04.5.art@gmail.com>
2023-02-09 08:53:16 +0200

    comit

---
+++ lesson-13/script.js
@@ -0,0 +1,13 @@
+const listItem = document.querySelectorAll('.item');
+console.log('Number of categories:', listItem.length);
+
+const animals = document.querySelectorAll('.item');
+const h2Number = document.querySelectorAll('h2');
+console.log('Category:', h2Number[0].textContent);
+console.log('Elements:', );
+
+console.log('Category:', h2Number[1].textContent);
+console.log('Elements:', );
+
+console.log('Category:', h2Number[2].textContent);
+console.log('Elements:', );
