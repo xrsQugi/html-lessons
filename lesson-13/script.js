21db52351ae7b4b118f4c9378199fd50a4ccf62d
xrsQugi <02.04.5.art@gmail.com>
2023-02-09 08:54:46 +0200

    com

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -1,8 +1,19 @@
+// Порахує і виведе в консоль кількість категорій 
+// в ul#categories, тобто елементів li.item.
+
 const listItem = document.querySelectorAll('.item');
 console.log('Number of categories:', listItem.length);
 
-const animals = document.querySelectorAll('.item');
+//!=========================================================
+
+// Для кожного элемента li.item 
+// у списку ul#categories, знайде і виведе 
+// в консоль текст заголовку елемента (тегу <h2>) 
+// і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
 const h2Number = document.querySelectorAll('h2');
+const animals = document.querySelectorAll('.item');
+
 console.log('Category:', h2Number[0].textContent);
 console.log('Elements:', );
 
