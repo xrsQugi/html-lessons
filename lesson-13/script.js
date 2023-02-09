1f41177b06af411500c349074adb0a871fe4cc9f
xrsQugi <02.04.5.art@gmail.com>
2023-02-09 09:00:44 +0200

    commit 13

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -1,8 +1,8 @@
 // Порахує і виведе в консоль кількість категорій 
 // в ul#categories, тобто елементів li.item.
 
-const listItem = document.querySelectorAll('.item');
-console.log('Number of categories:', listItem.length);
+// const listItem = document.querySelectorAll('.item');
+// console.log('Number of categories:', listItem.length);
 
 //!=========================================================
 
@@ -11,14 +11,22 @@ console.log('Number of categories:', listItem.length);
 // в консоль текст заголовку елемента (тегу <h2>) 
 // і кількість елементів в категорії (усіх <li>, вкладених в нього).
 
-const h2Number = document.querySelectorAll('h2');
-const animals = document.querySelectorAll('.item');
+// const h2Number = document.querySelectorAll('h2');
+// const animals = document.querySelectorAll('.item');
 
-console.log('Category:', h2Number[0].textContent);
-console.log('Elements:', );
+// console.log('Category:', h2Number[0].textContent);
+// console.log('Elements:', );
 
-console.log('Category:', h2Number[1].textContent);
-console.log('Elements:', );
+// console.log('Category:', h2Number[1].textContent);
+// console.log('Elements:', );
 
-console.log('Category:', h2Number[2].textContent);
-console.log('Elements:', );
+// console.log('Category:', h2Number[2].textContent);
+// console.log('Elements:', );
+
+
+const ul = document.querySelectorAll('#categories>li');
+for (const el of ul) {
+    console.log(
+        `Категория: ${el.firstElementChild.textContent},\nКоличество элементов: ${el.lastElementChild.children.length}`,
+    );
+}
