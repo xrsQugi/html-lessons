db47857618d64f0a8a7852f063a69f804836dac7
xrsQugi <02.04.5.art@gmail.com>
2023-02-09 15:24:13 +0200

    13

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -4,29 +4,30 @@
 // const listItem = document.querySelectorAll('.item');
 // console.log('Number of categories:', listItem.length);
 
-//!=========================================================
+//!================================================================
 
 // Для кожного элемента li.item 
 // у списку ul#categories, знайде і виведе 
 // в консоль текст заголовку елемента (тегу <h2>) 
 // і кількість елементів в категорії (усіх <li>, вкладених в нього).
 
-// const h2Number = document.querySelectorAll('h2');
-// const animals = document.querySelectorAll('.item');
+//!==========================First method==========================
+const h2Elem = document.querySelectorAll('h2');
+const elements = document.querySelectorAll('.item');
 
-// console.log('Category:', h2Number[0].textContent);
-// console.log('Elements:', );
+console.log('Category:', h2Elem[0].textContent);
+console.log('Elements:', elements[0].lastElementChild.children.length);
 
-// console.log('Category:', h2Number[1].textContent);
-// console.log('Elements:', );
+console.log('Category:', h2Elem[1].textContent);
+console.log('Elements:', elements[0].lastElementChild.children.length);
 
-// console.log('Category:', h2Number[2].textContent);
-// console.log('Elements:', );
+console.log('Category:', h2Elem[2].textContent);
+console.log('Elements:', elements[0].lastElementChild.children.length);
 
-
-const ul = document.querySelectorAll('#categories>li');
-for (const el of ul) {
-    console.log(
-        `Категория: ${el.firstElementChild.textContent},\nКоличество элементов: ${el.lastElementChild.children.length}`,
-    );
-}
+//!==========================Second method=========================
+// const ul = document.querySelectorAll('#categories>li');
+// for (const el of ul) {
+//     console.log(
+//         `Category: ${el.firstElementChild.textContent}\nElements: ${el.lastElementChild.children.length}`,
+//     );
+// }
