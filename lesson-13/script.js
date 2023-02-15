69a29b7e61de6b1decd0240c0a690524e8bbdc69
xrsQugi <02.04.5.art@gmail.com>
2023-02-15 09:14:36 +0200

    commit

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -1,3 +1,4 @@
+//?First task=============================================================================================================================
 //!========================First part==============================
 // Порахує і виведе в консоль кількість категорій 
 // в ul#categories, тобто елементів li.item.
@@ -31,3 +32,32 @@
 //         `Category: ${el.firstElementChild.textContent}\nElements: ${el.lastElementChild.children.length}`,
 //     );
 // }
+
+//?Second task============================================================================================================================
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+// <ul class="gallery"></ul>
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. 
+// Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+const images = [
+    {
+        url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+        alt: "White and Black Long Fur Cat",
+    },
+    {
+        url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+        alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    },
+    {
+        url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+        alt: "Group of Horses Running",
+    },
+];
+
+const gallery = document.querySelector('.gallery');
+
+const imageItems = images.map((image) =>`<li class="list-item"><img src="${image.url}" alt="${image.alt}" width = 300px height = 190px ></li>`).join('');
+gallery.insertAdjacentHTML("afterbegin", imageItems);
+gallery.style.listStyle = 'none';
