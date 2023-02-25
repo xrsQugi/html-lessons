883d18e4347dace050c2186ed837c0c2ddbc8b8b
xrsQugi <02.04.5.art@gmail.com>
2023-02-25 20:40:49 +0200

    commit

--- lesson-14/script.js
+++ lesson-14/script.js
@@ -19,3 +19,15 @@ decrement.addEventListener('click', minusOne);
 
 
 //! Завдання 2 ===================================
+
+const inputText = document.querySelector('#name-input');
+const outputText = document.querySelector('#name-output');
+
+const setOutput = (event) => {
+    outputText.textContent = event.currentTarget.value;
+    if (outputText.textContent.trim() === '') {
+        outputText.textContent = 'Anonymous';
+    }
+}
+
+inputText.addEventListener("input", setOutput);
