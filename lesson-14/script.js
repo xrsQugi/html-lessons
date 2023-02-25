7b89ea209ccc8d1fb2e5d54e61f41016c6607a85
xrsQugi <02.04.5.art@gmail.com>
2023-02-25 20:20:34 +0200

    commit

--- lesson-14/script.js
+++ lesson-14/script.js
@@ -1,4 +1,21 @@
-document.addEventListener("keydown", event => {
-    console.log("key: ", event.key);
-    console.log("code: ", event.code);
-  });
+//! Завдання 1 ===================================
+const counterInput = document.querySelector('#value');
+let counterValue = 0;
+
+const decrement = document.querySelector('[data-action="decrement"]');
+const increment = document.querySelector('[data-action="increment"]');
+
+const plusOne = () => {
+    counterValue += 1;
+    counterInput.textContent = counterValue;
+}
+increment.addEventListener('click', plusOne);
+
+const minusOne = () => {
+    counterValue -= 1;
+    counterInput.textContent = counterValue;
+}
+decrement.addEventListener('click', minusOne);
+
+
+//! Завдання 2 ===================================
