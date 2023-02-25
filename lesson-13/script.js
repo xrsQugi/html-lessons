4e04fe43f408ac66c9924bddb47ee95186215699
xrsQugi <02.04.5.art@gmail.com>
2023-02-25 19:18:01 +0200

    commit

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -13,17 +13,17 @@
 // і кількість елементів в категорії (усіх <li>, вкладених в нього).
 
 //todo==========================First method=======================
-// const h2Elem = document.querySelectorAll('h2');
-// const elements = document.querySelectorAll('.item');
+const h2Elem = document.querySelectorAll('h2');
+const elements = document.querySelectorAll('.item');
 
-// console.log('Category:', h2Elem[0].textContent);
-// console.log('Elements:', elements[0].lastElementChild.children.length);
+console.log('Category:', h2Elem[0].textContent);
+console.log('Elements:', elements[0].lastElementChild.children.length);
 
-// console.log('Category:', h2Elem[1].textContent);
-// console.log('Elements:', elements[1].lastElementChild.children.length);
+console.log('Category:', h2Elem[1].textContent);
+console.log('Elements:', elements[1].lastElementChild.children.length);
 
-// console.log('Category:', h2Elem[2].textContent);
-// console.log('Elements:', elements[2].lastElementChild.children.length);
+console.log('Category:', h2Elem[2].textContent);
+console.log('Elements:', elements[2].lastElementChild.children.length);
 
 //todo==========================Second method======================
 // const ul = document.querySelectorAll('#categories>li');
@@ -41,23 +41,25 @@
 // Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
 // Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
 
-const images = [
-    {
-        url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-        alt: "White and Black Long Fur Cat",
-    },
-    {
-        url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-        alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-    },
-    {
-        url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-        alt: "Group of Horses Running",
-    },
-];
+// const images = [
+//     {
+//         url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//         alt: "White and Black Long Fur Cat",
+//     },
+//     {
+//         url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//         alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//     },
+//     {
+//         url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//         alt: "Group of Horses Running",
+//     },
+// ];
 
-const gallery = document.querySelector('.gallery');
+// const gallery = document.querySelector('.gallery');
 
-const imageItems = images.map((image) =>`<li class="list-item"><img src="${image.url}" alt="${image.alt}" width = 300px height = 190px ></li>`).join('');
-gallery.insertAdjacentHTML("afterbegin", imageItems);
-gallery.style.listStyle = 'none';
+// const imageItems = images.map((image) =>`<li class="list-item"><img src="${image.url}" alt="${image.alt}" width = 300px height = 190px ></li>`).join('');
+// gallery.insertAdjacentHTML("afterbegin", imageItems);
+// gallery.style.listStyle = 'none';
+// gallery.style.display = 'flex';
+// gallery.style.gap = '20px';
---
+++ lesson-14/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("keydown", event => {
+    console.log("key: ", event.key);
+    console.log("code: ", event.code);
+  });
